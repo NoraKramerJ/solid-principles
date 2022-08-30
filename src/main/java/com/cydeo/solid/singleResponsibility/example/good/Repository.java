3c9bcb92028a76294d221a5d04dca2214cd6d08e
Nmerkel <noramerkel0@gmail.com>
2022-08-30 12:46:13 -0400

    created Repository class

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/Repository.java
@@ -0,0 +1,25 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Repository {
+
+    public static void create(){
+        //Connect to database
+        //Create user in database
+
+    }
+    public static void read(){
+        //Connect to database
+        //read user from database
+
+    }
+    public static void update() {
+        //Connect to database
+        //update user from database
+
+    }
+    public static void delete() {
+        //Connect to database
+        //delete user from database
+    }
+
+}
