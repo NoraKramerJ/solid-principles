a3eb23f53c3b6b64cc1798685973d7192547c036
Nmerkel <noramerkel0@gmail.com>
2022-08-30 12:42:11 -0400

    created a CRUDService class

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -0,0 +1,9 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class CRUDService {
+    public static void create(){
+        //create a user
+        // send it to database related part
+
+    }
+}
