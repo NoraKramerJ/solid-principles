3955cb6d34916368b7569a366cb93533f387c8ce
Nmerkel <noramerkel0@gmail.com>
2022-08-30 12:40:36 -0400

    created a SecurityFilter class

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/SecurityFilter.java
@@ -0,0 +1,14 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class SecurityFilter {
+
+    public static boolean authenticate(){
+        //check authentication
+        return true;
+
+    }
+    public static boolean authorization(){
+        //check authorization
+        return true;
+    }
+}
