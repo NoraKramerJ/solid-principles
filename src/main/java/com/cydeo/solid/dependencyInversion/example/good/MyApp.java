67c0dcd357c4f2b986547dec90aca4f18aa31c17
Nmerkel <noramerkel0@gmail.com>
2022-08-30 15:46:02 -0400

    creating two new classes and uses the same abstract method from the interface

--- src/main/java/com/cydeo/solid/dependencyInversion/example/good/MyApp.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/example/good/MyApp.java
@@ -2,5 +2,10 @@ package com.cydeo.solid.dependencyInversion.example.good;
 
 public class MyApp {
 
+    public static void read(Reader reader){
 
+
+        UserReader userReader=new UserReader(reader);
+        userReader.getUserName();
+    }
 }
